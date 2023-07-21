ad0dccd54d4006c8ced0bbbbcb1dd5e521ebe6c4
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-07-21 14:26:51 +1000

    Looping Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let names = [];
+
+function writeCards(names, event = "surprise") {
+    let i = 0;
+    let testArr = [];
+    while (i < names.length) {
+        let nameString = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        testArr.push(nameString);
+        i++;
+    }
+    return testArr;
+}
+console.log(writeCards([`Guadalupe`, `Ollie`, `Aki`]));
+
+function countDown (number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+console.log(countDown(10))
